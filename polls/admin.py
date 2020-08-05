44a24fab451701989a444488500397f18f0df165
denghongbo <hongbo.deng@semioe.com>
2020-08-05 14:38:29 +0800

    update

--- polls/admin.py
+++ polls/admin.py
@@ -9,6 +9,7 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_per_page = 10
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
@@ -16,6 +17,10 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
 
 
+class ChoiceAdmin(admin.ModelAdmin):
+    list_per_page = 15
+
+
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+admin.site.register(Choice, ChoiceAdmin)
 
